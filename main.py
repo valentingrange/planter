390b0eb488602d8eb3b7ee6e5dea16f468f106c8
valentingrange <valentin.grange.2014@gmail.com>
2025-03-16 19:16:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ largeur_personnage = 50
 hauteur_personnage = 50
 x_personnage = largeur_fenetre // 2 - largeur_personnage // 2
 y_personnage = hauteur_fenetre - hauteur_personnage - 50  # Position du personnage sur la plateforme
-vitesse = 100  # Vitesse de déplacement du personnage
+vitesse = 50  # Vitesse de déplacement du personnage
 
 # Créer un canevas pour dessiner
 canvas = tk.Canvas(root, width=largeur_fenetre, height=hauteur_fenetre)
@@ -40,9 +40,25 @@ def deplacer_droite(event):
         x_personnage += vitesse
         canvas.coords(personnage, x_personnage, y_personnage, x_personnage + largeur_personnage, y_personnage + hauteur_personnage)
 
+# Déplacer le personnage vers le haut (saut)
+def deplacer_haut(event):
+    global y_personnage
+    if y_personnage > 0:
+        y_personnage -= vitesse  # Décale vers le haut
+        canvas.coords(personnage, x_personnage, y_personnage, x_personnage + largeur_personnage, y_personnage + hauteur_personnage)
+
+# Déplacer le personnage vers le bas
+def deplacer_bas(event):
+    global y_personnage
+    if y_personnage < hauteur_fenetre - hauteur_personnage - 50:  # Limite à la plateforme
+        y_personnage += vitesse  # Décale vers le bas
+        canvas.coords(personnage, x_personnage, y_personnage, x_personnage + largeur_personnage, y_personnage + hauteur_personnage)
+
 # Associer les touches du clavier aux actions
 root.bind("<Left>", deplacer_gauche)
 root.bind("<Right>", deplacer_droite)
+root.bind("<Up>", deplacer_haut)  # Touche pour déplacer vers le haut
+root.bind("<Down>", deplacer_bas)  # Touche pour déplacer vers le bas
 
 # Lancer la fenêtre
 root.mainloop()
